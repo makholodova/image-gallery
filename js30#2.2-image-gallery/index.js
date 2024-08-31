089b8064c280b3c73e28be8aa875dae32e5d1440
Marina Kholodova <marinaholodova@gmail.com>
2024-08-31 23:30:50 +0300

    feat: add functions show and creat img

--- js30#2.2-image-gallery/index.js
+++ js30#2.2-image-gallery/index.js
@@ -3,6 +3,25 @@ const url = "https://api.unsplash.com/search/photos?query=spring&per_page=30&ori
 async function getData() {
 	const res = await fetch(url);
 	const data = await res.json();
+	showData(data);
 	//console.log(data.results[0].urls.regular); //
 }
+
+const galleryContainer = document.querySelector('.gallery');
+
+function showData(data) {
+	data.results.map(item => {
+		createImg(item);
+	});
+
+
+}
+
+function createImg(element) {
+	const img = document.createElement('div');
+	img.classList.add("gallery__img");
+	img.style.backgroundImage = `url(${element.urls.regular})`;
+	galleryContainer.append(img);
+}
+
 getData();
